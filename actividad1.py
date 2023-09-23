5269c4e9ad6420aea94189a41111db5c006c1fd0
Loury Mijares <lourymivi@gmail.com>
2023-09-22 21:48:38 -0500

    workbook

---
+++ actividad1.py
@@ -0,0 +1,5 @@
+num= int(input("Ingresa tu edad: "))
+if num > 18:
+    print("Es mayor de edad")
+else:
+    print("Es menor de edad")
---
+++ actividad2.py
@@ -0,0 +1,5 @@
+num= int(input("Ingresa un numero entero: "))
+if num % 2 == 0:
+    print(num, "es par.")
+else:
+    print(num, "es impar. ")
