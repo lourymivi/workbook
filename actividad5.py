b9b197e70cbdd14989745dadc5d01d8fe67920bc
Loury Mijares <lourymivi@gmail.com>
2023-09-25 21:09:34 -0500

    workbook

---
+++ actividad5.py
@@ -0,0 +1,8 @@
+caracteres = input("Escribe los caracteres: ")
+
+lista_frutas = [caracteres]
+
+for i in range(len(lista_frutas)):
+ print(i, lista_frutas[i])
+for x in range(len(lista_frutas[i])):
+ print(lista_frutas[i][x])
---
+++ actividad6.py
@@ -0,0 +1,28 @@
+palabras1 = input("ingresa una lista de palabras:  ")
+palabras2 = input("ingresa una lista de palabras:  ")
+palabras3 = input("ingresa una lista de palabras:  ")
+letra_p = input("Indica tú letra: ")
+
+for i in range(len(palabras1)):
+  for i in range(len(palabras2)):
+    for i in range(len(palabras3)):
+      letra1 = palabras1[i - i]
+      letra2 = palabras2[i - i]
+      letra3 = palabras3[i - i]
+
+if letra_p == letra1 and letra2 and letra3:
+  print(palabras1, palabras2, palabras3)
+elif letra_p == letra1 and letra2:
+  print(palabras1, palabras2)
+elif letra_p == letra1 and letra3:
+  print(palabras1, palabras3)
+elif letra_p == letra2 and letra3:
+  print(palabras2, palabras3)
+elif letra_p == letra1:
+  print(palabras1)
+elif letra_p == letra2:
+  print(palabras2)
+elif letra_p == letra3:
+  print(palabras3)
+else:
+  print("No hay palabra con esta letra")
